f9f551ead23d35c9572b3a494fc7c32d71c97dd5
Nikhil <chrsnikhil@gmail.com>
2025-02-22 01:38:30 +0530

    MOOOOOOOOOOOOOOOORE LINTING ERRORS

--- app/parking/[id]/page.tsx
+++ app/parking/[id]/page.tsx
@@ -1,14 +1,13 @@
-"use client"
-
 import ParkingSpaceBooking from '@/components/parkingspacebooking'
 import ParkingSpaceBookingForPayment from '@/components/parkingspacebookingforpayment'
 
-interface PageProps {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export default function ParkingPage({ params, searchParams }: PageProps) {
+export default function ParkingPage({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const locationName = searchParams.name as string
   const totalSpaces = parseInt(searchParams.totalSpaces as string)
   const isSensorEnabled = searchParams.isSensorEnabled === 'true'
